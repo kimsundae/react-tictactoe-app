8a02fa89614ee0123cb7ea3e623b1e08ad1ac048
kimgeunbae <naa159@naver.com>
2024-11-04 23:18:19 +0900

    리액트 훅스

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,27 +2,25 @@ import React, { Component } from "react";
 import "./ExpenseItem.css";
 import { MdEdit, MdDelete } from "react-icons/md";
 
-export class ExpenseItem extends Component {
-  render() {
-    return (
-      <li className="item">
-        <div className="info">
-          <span className="expnse">{this.props.expense.charge}</span>
-          <span className="amount">{this.props.expense.amount}</span>
-        </div>
-        <div>
-          <button className="edit-btn">
-            수정
-            <MdEdit />
-          </button>
-          <button className="clear-btn">
-            삭제
-            <MdDelete />
-          </button>
-        </div>
-      </li>
-    );
-  }
-}
+const ExpenseItem = ({ expense, handleDelete }) => {
+  return (
+    <li className="item">
+      <div className="info">
+        <span className="expnse">{expense.charge}</span>
+        <span className="amount">{expense.amount}</span>
+      </div>
+      <div>
+        <button className="edit-btn">
+          수정
+          <MdEdit />
+        </button>
+        <button className="clear-btn" onClick={() => handleDelete(expense.id)}>
+          삭제
+          <MdDelete />
+        </button>
+      </div>
+    </li>
+  );
+};
 
 export default ExpenseItem;
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,24 +1,26 @@
-import React, { Component } from "react";
 import "./ExpenseList.css";
 import ExpenseItem from "./ExpenseItem";
 import { MdDelete } from "react-icons/md";
-export class ExpenseList extends Component {
-  render() {
-    console.log(this.props.initialExpenses);
-    return (
-      <>
-        <ul className="list">
-          {this.props.initialExpenses.map((expense) => {
-            return <ExpenseItem expense={expense} key={expense.id} />;
-          })}
-        </ul>
-        <button className="btn">
-          목록 지우기
-          <MdDelete className="btn-icon" />
-        </button>
-      </>
-    );
-  }
-}
+const ExpenseList = ({ handleDelete, initialExpenses }) => {
+  return (
+    <>
+      <ul className="list">
+        {initialExpenses.map((expense) => {
+          return (
+            <ExpenseItem
+              handleDelete={handleDelete}
+              expense={expense}
+              key={expense.id}
+            />
+          );
+        })}
+      </ul>
+      <button className="btn">
+        목록 지우기
+        <MdDelete className="btn-icon" />
+      </button>
+    </>
+  );
+};
 
 export default ExpenseList;
