7306814df4e74c9bc40c99fa74aaed05d7f5f749
kimgeunbae <naa159@naver.com>
2024-11-06 00:00:40 +0900

    리액트 지출목록추가 기능

--- src/App.js
+++ src/App.js
@@ -4,21 +4,51 @@ import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 
 const App = () => {
+  const [charge, setCharge] = useState("");
+
+  const [amount, setAmount] = useState(0);
+
   const [expenses, setExpenses] = useState([
     { id: 1, charge: "렌트비", amount: 1600 },
     { id: 2, charge: "교통비", amount: 400 },
     { id: 3, charge: "식비", amount: 1200 },
   ]);
 
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
+  };
+  const handleAmount = (e) => {
+    setAmount(e.target.value);
+  };
+
   const handelDelete = (id) => {
     const newExpenses = expenses.filter((expense) => expense.id !== id);
     setExpenses(newExpenses);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (charge !== "" && amount > 0) {
+      const newExpense = { id: crypto.randomUUID(), charge, amount };
+      const newExpenses = [...expenses, newExpense];
+      setExpenses(newExpenses);
+      setCharge("");
+      setAmount(0);
+    } else {
+      console.log("error");
+    }
   };
   return (
     <main className="main-container">
       <h1>예산 계산기</h1>
       <div style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}>
-        <ExpenseForm />
+        <ExpenseForm
+          handleCharge={handleCharge}
+          charge={charge}
+          amount={amount}
+          handleAmount={handleAmount}
+          handleSubmit={handleSubmit}
+        />
       </div>
 
       <div style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}>
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -2,9 +2,15 @@ import React, { Component } from "react";
 import { MdSend } from "react-icons/md";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = ({
+  handleCharge,
+  charge,
+  handleAmount,
+  amount,
+  handleSubmit,
+}) => {
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="form-center">
         <div className="form-group">
           <label htmlFor="charge">지출 항목</label>
@@ -13,7 +19,9 @@ const ExpenseForm = () => {
             className="form-control"
             id="charge"
             name="charge"
+            value={charge}
             placeholder="예) 렌트비"
+            onChange={handleCharge}
           />
         </div>
         <div className="form-group">
@@ -24,6 +32,8 @@ const ExpenseForm = () => {
             id="amount"
             name="amount"
             placeholder="예) 100"
+            value={amount}
+            onChange={handleAmount}
           />
         </div>
       </div>
